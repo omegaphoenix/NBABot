9fb56f3ad59479a15f18d1936cdacbe8aa9875f4
Justin Leong <justinleong16@gmail.com>
2017-03-30 00:58:55 -0700

    Add new bot to play three point contests

---
+++ threes.py
@@ -0,0 +1,36 @@
+"""Imports the monkeyrunner modules used by this program"""
+from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
+
+SHOT_TIME = 0.54
+PAUSE = 1.97
+
+def shoot():
+    """Shoot a shot"""
+    MonkeyRunner.sleep(PAUSE)
+    start = (1850, 750)
+    DEVICE.drag(start, start, SHOT_TIME)
+
+def shoot_rack():
+    """Shoot 5 shots"""
+    shoot()
+    shoot()
+    shoot()
+    shoot()
+    shoot()
+
+def shoot_round():
+    """Shoot 25 shots"""
+    shoot_rack()
+    shoot_rack()
+    shoot_rack()
+    shoot_rack()
+    shoot_rack()
+
+print "Waiting for device!"
+# Connects to the current device, returning a MonkeyDevice object
+DEVICE = MonkeyRunner.waitForConnection()
+print "Connected to device!"
+DEVICE.touch(1850, 750, 'DOWN_AND_UP')
+MonkeyRunner.sleep(1.1)
+print "Begin Shooting!"
+shoot_round()
